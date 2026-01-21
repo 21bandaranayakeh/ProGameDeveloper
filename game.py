48aac482ba35abd62678caf2f0ac87933fa23808
21bandaranayakeh <hirukabandaranayake@gmail.com>
2026-01-21 17:58:48 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -19,3 +19,34 @@ BULLET_FIRE_SOUND = pygame.mixer.Sound(Gun+Silencer.mp3)
 
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+
+FPS = 60
+VEL = 5
+BULLET_VEL = 7
+MAX_BULLETS = 3
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+#24
+YELLOW_HIT = pygame.USEREVENT + 1 #25
+RED_HIT - pygame.USEREVENT + 2 #26
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(
+    pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(
+    pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('space.png')), (WIDTH, HEIGHT))
+
+def draw_window(red, yellow , red_bullets, yellow_bullets, red_health, yellow_health):
+    WIN.blit(SPACE, (0, 0))
+    pygame.draw.rect(WIN, BLACK, BORDER)
+
+    red_health_text = HEALTH_FONT.render('Health: '+str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render('Health: '+str(yellow_health), 1, WHITE)
+
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+    
