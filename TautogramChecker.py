a07518ea0afbf5eeca9e2020ed9c54963f887cfe
21bandaranayakeh <hirukabandaranayake@gmail.com>
2026-03-18 17:07:46 +0000

    Add files via upload

--- TautogramChecker.py
+++ TautogramChecker.py
@@ -1,23 +1,34 @@
-a = str(input('Please enter a string.   '))
-
-a = a.lower()
-
-b = ''
+a = 0
+c = 0
 l = []
+
+while a != '-1':
+    a = str(input('Please enter a string.   '))
+    c += 1
+
+    a = a.lower()
+    if a != '-1':
+        l.append(a)
+
+
+
+n = 0
+b = ''
 count = 0
 count2 = 0
+count3 = 0
 
+for i in l:
+    for j in range(len(l[n])):
+        if l[n][j] == ' ':
+            count += 1
+            if l[n][j+1] == l[n][0]:
+                count2 += 1
 
-for i in a:
-    l.append(i)
+    if count == count2:
+        count3 += 1
+    count = 0
+    count2 = 0
+    n += 1
 
-for i in range(len(l)):
-    if l[i] == ' ':
-        count += 1
-        if l[i+1] == l[0]:
-            count2 += 1
-
-if count == count2:
-    print('Your string is a tautogram.   ')
-else:
-    print('Your string is not a tautogram.   ')
+print('You have entered a total of {} tautograms.   '.format(count3))
